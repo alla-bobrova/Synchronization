87371a77131bfa5f0ce434b3719dbbd022213211
alla-bobrova <alla_bobrova.31@mail.ru>
2024-02-05 21:12:14 +0300

    new page

---
+++ app/pages/Phase reduction.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Phase reduction", page_icon=("app/icons/phase_reduction.png"))
+
+st.sidebar.title("Phase reduction")
+ 
+st.title("Phase reduction")
+
