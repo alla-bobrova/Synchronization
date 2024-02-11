dd099b822893eaa3d8056ab725e83ce0c3197182
alla-bobrova <alla_bobrova.31@mail.ru>
2024-02-11 18:17:53 +0300

    Update Main.py

--- app/Main.py
+++ app/Main.py
@@ -23,11 +23,6 @@ def main():
     st.write("Моделирование уравнения фаз глобально связанных осцилляторов Ван дер Поля. Связи между осцилляторами заданы матрицей $N\\times N$, где $R_{ij}=0$ при $i=j$.")
 
     st.subheader("Results")
-    st.write("В данных файлах содержатся результаты моделирования для:")
-    st.write("- $\\gamma=\\displaystyle\\frac{\\pi}{4}$: `piDiv4.ipynb`")
-    st.write("- $\\gamma=\\displaystyle\\frac{\\pi}{2}$: `piDiv2.ipynb`")
-    st.write("- $\\gamma=\\displaystyle\\frac{3\\pi}{4}$: `3piDiv4.ipynb`")
-    st.write("- $\\gamma=\\pi$: `pi.ipynb`")
     st.write("Результаты для `R - matrix.ipynb` при единичных связях и значениях собственных частот совпадают с `R - one-dimensional array.ipynb`.")
 
 
